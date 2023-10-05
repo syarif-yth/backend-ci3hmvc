2c291d209717a66138425d53d8841b8f0ba19779
syarif-yth <syarif.yth@gmail.com>
2023-10-06 00:09:58 +0700

    initail commit

--- application/modules/Migrate/controllers/Migrate.php
+++ application/modules/Migrate/controllers/Migrate.php
@@ -10,27 +10,23 @@ class Migrate extends MX_Controller
 		$this->load->helper('tree_helper');
   }
 
-	private function check_db()
-	{
-		$this->load->database();
-		$this->load->dbutil();
-		$db_name = $this->db->database;
-		if(!$this->dbutil->database_exists($db_name)) {
-			$res['status'] = false;
-			$res['message'] = 'Not connected database, or database not exists';
-		} else {
-			$res['status'] = true;
-			$res['message'] = 'Connected to '.$db_name;
-		}
-		return $res;
-	}
-
 	public function index()
 	{
 		$this->load->database();
 		$db_name = $this->db->database;
 		$view = array('dbName' => $db_name);
 		$this->load->view('migrate', $view);
+	}
+
+	private function count_table()
+	{
+		$jml = 0;
+		$path = APPPATH.'migrations/';
+		$glob = glob($path."*_*.php");
+		foreach($glob as $file) {
+			$jml++;
+		}
+		return $jml;
 	}
 
 	public function process()
@@ -42,9 +38,9 @@ class Migrate extends MX_Controller
 
 	private function migration()
 	{
-		$version = 3;
+		$version = $this->count_table();
 		$migrate = array();
-		for($v=0; $v<=$version; $v++) {
+		for($v=1; $v<=$version; $v++) {
 			if(!$this->migration->version($v)) {
 				$error = show_error($this->migration->error_string());
 				$migrate[] = array(
